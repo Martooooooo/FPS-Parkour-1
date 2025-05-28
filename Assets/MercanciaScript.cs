da6bbd2626c7e14fe0a8f657db375683af066442
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2025-05-28 15:47:57 -0300

    colisiones hecho

---
+++ Assets/MercanciaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+
+    public int precio;
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
